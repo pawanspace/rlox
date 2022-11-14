a3451564e36f42daca88084ff71752949668c62d
Pawan Chopra <pawanc@hey.com>
2022-11-13 20:07:03 -0800

    Adding Tombstone with delete operation and some tests.

--- src/hash_map.rs
+++ src/hash_map.rs
@@ -4,6 +4,7 @@ use std::fmt::Debug;
 pub(crate) enum Entry<T> {
     Occupied(String, T),
     Vacant,
+    TombStone,
 }
 
 #[derive(Debug)]
@@ -47,9 +48,30 @@ where
         self.find_entry(key)
     }
 
+    pub(crate) fn delete(&mut self, key: &str) -> Option<T> {
+        let bucket = self.find_bucket(key, &self.entries);
+        let value = self.get_at_index(bucket);
+        if value.is_some() {
+            self.insert_tombstone(bucket);
+        }
+        value
+    }
+
+    fn insert_tombstone(&mut self, bucket: usize) {
+        self.entries[bucket] = Entry::TombStone;
+    }
+
+    fn get_at_index(&mut self, bucket: usize) -> Option<T> {
+        let entry = &self.entries[bucket];
+        return match entry {
+            Entry::Occupied(_, value) => Some(value.clone()),
+            _ => None,
+        };
+    }
+
     fn ensure_capacity(&mut self) {
-        if (self.size / self.capacity) * 100 > self.load_factor {
-            self.capacity *= 2 + 1;
+        if ((self.size + 1) / self.capacity) * 100 > self.load_factor {
+            self.capacity = (self.capacity * 2) + 1;
             let mut temp_entries: Vec<Entry<T>> = vec![];
             temp_entries.resize(self.capacity, Entry::Vacant);
             self.size = 0;
@@ -60,7 +82,7 @@ where
                         temp_entries[bucket] = Entry::Occupied(key.to_string(), value.clone());
                         self.size += 1;
                     }
-                    Entry::Vacant => (),
+                    _ => (),
                 }
             }
 
@@ -83,6 +105,7 @@ where
     pub(crate) fn dump(&mut self) {
         println!("{:?}", self.entries);
     }
+
     fn find_entry(&self, key: &str) -> Option<&T> {
         let h = self.hash(key);
         let mut bucket = h % (self.capacity as u32);
@@ -98,6 +121,10 @@ where
                     }
                 }
                 Entry::Vacant => None,
+                Entry::TombStone => {
+                    bucket = (bucket + 1) % (self.capacity as u32);
+                    continue;
+                },
             };
         }
     }
@@ -112,7 +139,7 @@ where
                     true
                 }
             }
-            Entry::Vacant => false,
+            Entry::Vacant | Entry::TombStone => false,
         }
     }
 
@@ -125,5 +152,50 @@ where
             hash = hash.wrapping_mul(16777619);
         }
         hash
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn can_hold_multiple_keys() {
+        let mut map = Table::init(2);
+        map.insert("one", true);
+        map.insert("two", true);
+        assert!(map.size == 2);
+    }
+
+    #[test]
+    fn can_hold_and_return_multiple_keys() {
+        let mut map = Table::init(2);
+        map.insert("one", true);
+        map.insert("two", false);
+
+        assert_eq!(map.get("one"), Some(&true));
+        assert_eq!(map.get("two"), Some(&false));
+    }
+
+    #[test]
+    fn can_hold_and_delete_multiple_keys() {
+        let mut map = Table::init(2);
+        map.insert("one", true);
+        map.insert("two", false);
+        assert_eq!(map.get("one"), Some(&true));
+
+        map.delete("one");
+        assert_eq!(map.get("one"), None);
+    }
+
+    #[test]
+    fn can_expand_capacity_as_required() {
+        let mut map = Table::init(1);
+        map.insert("one", Some(&true));
+        assert_eq!(map.capacity, 3);
+        map.insert("two", Some(&false));
+        assert_eq!(map.capacity, 3);
+        map.insert("three", Some(&false));
+        assert_eq!(map.capacity, 7);
     }
 }
