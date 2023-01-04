ebd8d7ff29cb2872b3779e2b1bf692cf46d730ce
Pawan Chopra <pawanc@amazon.com>
2023-01-04 13:52:52 -0800

    removing child crates from main branch and keeping it in improved branch

--- compiler/src/compiler.rs
+++
@@ -1,9 +0,0 @@
-pub(crate) struct Compiler<'c> {
-    table: &'c mut Table<Value>,
-    scanner: Scanner,
-    parser: Parser,
-    source: String,
-    current_context: usize,
-    scope_depth: usize,
-    contexts: Vec<CompilerContext>
-}
